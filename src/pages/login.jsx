4b88f0e25430ed03f4c7f1de6e0d138ce2460bd8
Franky cunza chavez <franky.cunza@gmail.com>
2021-06-30 15:00:32 -0500

    changes

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,18 +1,74 @@
+import React, { useReducer, useState, useEffect } from 'react';
+import axios from 'axios'
+
+const formReducer = (state, event) => {
+    return {
+      ...state,
+      [event.name]: event.value
+    }
+}
+
 const Login = () => {
+
+    const [formData, setFormData] = useReducer(formReducer, {});
+    const [submitting, setSubmitting] = useState(false);
+
+    useEffect(() => {
+        
+    })
+
+    const handleChange = event => {
+        setFormData({
+          name: event.target.name,
+          value: event.target.value,
+        });
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        // setSubmitting(true);
+
+        const param = {
+            user: formData.user,
+            password: formData.password,
+            social: false
+        }
+
+        axios({
+            method: 'get',
+            url: 'https://gateway.vim365.com/users/checkuser',
+            headers: {
+                'security-header': 'Vim365Aputek/2020.04'
+            },
+            params: param
+        }).then(data => {
+            console.log(data.data)
+            for (const [key, value] of Object.entries(data.data)) {
+                localStorage.setItem(key, value)
+            }
+        })
+
+        // setTimeout(() => {
+        //     setSubmitting(false);
+        // }, 3000);
+    }
     return (
         <div className="w-full text-center mx-auto bg-blue-500 h-screen">
+            {submitting &&
+                <div>Submtting Form...</div>
+            }
             <div>
                 <h1 className="text-4xl font-semibold py-8 text-white">Iniciar sesión</h1>
             </div>
-            <form action="#" className="max-w-2xl bg-white shadow rounded p-8 mx-auto">
+            <form onSubmit={handleSubmit}  className="max-w-xl bg-white shadow rounded p-8 mx-auto">
                 <div className="text-left">
-                    <label htmlFor="" className="text-xl font-medium block">Usuario</label>
-                    <input type="text" className="h-12 w-full rounded mt-2 pl-4 bg-gray-200" />
+                    <label htmlFor="" className="text-lg font-medium block">Usuario</label>
+                    <input type="text" name="user" onChange={handleChange} className="h-12 w-full rounded mt-1 pl-4 bg-gray-200" />
                 </div>
 
                 <div className="text-left mt-4">
-                    <label htmlFor="" className="text-xl font-medium block">Contraseña</label>
-                    <input type="text" className="h-12 w-full rounded mt-2 pl-4 bg-gray-200" />
+                    <label htmlFor="" className="text-lg font-medium block">Contraseña</label>
+                    <input type="password" name="password" onChange={handleChange} className="h-12 w-full rounded mt-1 pl-4 bg-gray-200" />
                 </div>
 
                 <div className="text-left mt-4 flex justify-center w-full">
