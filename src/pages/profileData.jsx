e6f0f08980f9e61910e9cf9a6c71f57b465be356
Franky cunza chavez <franky.cunza@gmail.com>
2021-06-30 18:44:20 -0500

    changes

--- src/pages/profileData.jsx
+++ src/pages/profileData.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 const Profile = () => {
     const [data, setData] = useState([]);
+    const [profile, setProfile] = useState([]);
     useEffect(() => {
         const param = {
             company_id: localStorage.getItem('company_id'),
@@ -21,9 +22,25 @@ const Profile = () => {
             params: param
         }).then(response => {
             setData(response.data.data)
-            console.log(response.data.data)
+            // console.log(response.data.data)
+            getProfile()
         }, [])
     }, []);
+
+    const getProfile = () => {
+        axios({
+            method: 'get',
+            url: 'https://gateway.vim365.com/users/getprofile',
+            headers: {
+                'security-header': 'Vim365Aputek/2020.04',
+                Authorization: localStorage.getItem('token'),
+                id: localStorage.getItem('id')
+            },
+        }).then(response => {
+            console.log(response)
+            setProfile(response.data.data.data)
+        }, [])
+    }
 
     return (
         <div>
@@ -31,12 +48,12 @@ const Profile = () => {
                 {data.map((item, i) => {
                     if (item.type == "document") {
                         return (
-                            <div className="mt-2 text-left w-full">
+                            <div className="mt-2 text-left w-full" key={i}>
                                 <label htmlFor="" className="block mb-1">{item.title}</label>
                                 <div className="flex" key={i}>
                                     {item.loop.map((loop, d) => {
                                         return(
-                                            <div className={`bg-gray-200 rounded px-6 py-3 flex w-max ${d == 0 ? '' : 'ml-2'}`}>{loop}</div>
+                                            <div className={`bg-gray-200 rounded px-6 py-3 flex w-max ${d == 0 ? '' : 'ml-2'}`} key={d}>{loop}</div>
                                         )
                                     })}
                                 </div>
@@ -46,14 +63,14 @@ const Profile = () => {
                         return (
                             <div className="mt-2 text-left w-full" key={i}>
                                 <label htmlFor="" className="block mb-1">{item.title}</label>
-                                <input type="text" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                                <input type="text" name="" id="" defaultValue={profile[item.form_type]} className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
                             </div>
                         )
                     } else if (item.form_type == "birth_day") {
                         return (
                             <div className="mt-2 text-left w-full" key={i}>
                                 <label htmlFor="" className="block mb-1">{item.title}</label>
-                                <input type="date" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                                <input type="date" name="" id="" defaultValue={profile[item.form_type]} className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
                             </div>
                         )
                     } else if (item.form_type == "gender") {
@@ -67,14 +84,14 @@ const Profile = () => {
                         return (
                             <div className="mt-2 text-left w-full" key={i}>
                                 <label htmlFor="" className="block mb-1">{item.title}</label>
-                                <input type="number" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                                <input type="number" name="" id="" defaultValue={profile[item.form_type]} className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
                             </div>
                         )
                     } else if (item.form_type == "gender") {
                         return (
                             <div className="mt-2 text-left w-full" key={i}>
                                 <label htmlFor="" className="block mb-1">{item.title}</label>
-                                <input type="text" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                                <input type="text" name="" id="" defaultValue={profile[item.form_type]} className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
                             </div>
                         )
                     } else if (item.form_type == "email") {
@@ -88,7 +105,7 @@ const Profile = () => {
                         return (
                             <div className="mt-2 text-left w-full" key={i}>
                                 <label htmlFor="" className="block mb-1">{item.title}</label>
-                                <input type="address" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                                <input type="address" name="" id="" defaultValue={profile[item.form_type]} className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
                             </div>
                         )
                     } else if (item.type == "title") {
@@ -101,28 +118,28 @@ const Profile = () => {
                         return (
                             <div className="mt-2 text-left w-full" key={i}>
                                 <label htmlFor="" className="block mb-1">{item.title}</label>
-                                <input type="address" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                                <input type="address" name="" id="" defaultValue={profile[item.form_type]} className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
                             </div>
                         )
                     } else if (item.form_type == "work_address") {
                         return (
                             <div className="mt-2 text-left w-full" key={i}>
                                 <label htmlFor="" className="block mb-1">{item.title}</label>
-                                <input type="address" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                                <input type="address" name="" id="" defaultValue={profile[item.form_type]} className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
                             </div>
                         )
                     } else if (item.form_type == "work_area") {
                         return (
                             <div className="mt-2 text-left w-full" key={i}>
                                 <label htmlFor="" className="block mb-1">{item.title}</label>
-                                <input type="address" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                                <input type="address" name="" id="" defaultValue={profile[item.form_type]} className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
                             </div>
                         )
                     } else if (item.form_type == "work_name") {
                         return (
                             <div className="mt-2 text-left w-full" key={i}>
                                 <label htmlFor="" className="block mb-1">{item.title}</label>
-                                <input type="address" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                                <input type="address" name="" id="" defaultValue={profile[item.form_type]} className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
                             </div>
                         )
                     }
