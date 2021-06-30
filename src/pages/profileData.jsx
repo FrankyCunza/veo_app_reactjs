23ed924ad9f62fffae26b1d41a465958ec8e85db
Franky cunza chavez <franky.cunza@gmail.com>
2021-06-30 17:36:02 -0500

    changes

--- src/pages/profileData.jsx
+++ src/pages/profileData.jsx
@@ -27,16 +27,25 @@ const Profile = () => {
 
     return (
         <div>
-            Hello World!
             <div className="max-w-xl mx-auto">
-                {data.map((item, i) => (
-                    (item.form_type == "text" ? (
-                        <div className="mt-2 text-left w-full">
-                            <label htmlFor="" className="block mb-1">{item.title}</label>
-                            <input type="text" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
-                        </div>
-                    ) : (''))
-                ))}
+                {data.map((item, i) => {
+                    if (item.type == "document") {
+                        return (
+                            item.loop.map((loop) => {
+                                return(
+                                    <div className="bg-gray-200 rounded px-6 py-3 flex w-max">{loop}</div>
+                                )
+                            })
+                        )
+                    } else if (item.form_type == "text") {
+                        return (
+                            <div className="mt-2 text-left w-full" key={i}>
+                                <label htmlFor="" className="block mb-1">{item.title}</label>
+                                <input type="text" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                            </div>
+                        )
+                    }
+                })}
             </div>
         </div>
     )
