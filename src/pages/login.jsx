ee50f809962af3c8b4c3527e856bb9b0312fb116
Franky Cunza <franky.cunza@prix.tips>
2021-06-30 13:38:31 -0500

    changes

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,25 @@
 const Login = () => {
     return (
-        <h1 className="text-3xl text-red-600">"Hello Login!"</h1>
+        <div className="w-full text-center mx-auto bg-blue-500 h-screen">
+            <div>
+                <h1 className="text-4xl font-semibold py-8 text-white">Iniciar sesión</h1>
+            </div>
+            <form action="#" className="max-w-2xl bg-white shadow rounded p-8 mx-auto">
+                <div className="text-left">
+                    <label htmlFor="" className="text-xl font-medium block">Usuario</label>
+                    <input type="text" className="h-12 w-full rounded mt-2 pl-4 bg-gray-200" />
+                </div>
+
+                <div className="text-left mt-4">
+                    <label htmlFor="" className="text-xl font-medium block">Contraseña</label>
+                    <input type="text" className="h-12 w-full rounded mt-2 pl-4 bg-gray-200" />
+                </div>
+
+                <div className="text-left mt-4 flex justify-center w-full">
+                    <button type="submit" className="px-10 w-full bg-blue-600 rounded text-white py-3 text-lg">Enviar</button>
+                </div>
+            </form>
+        </div>
     )
 }
 
