06d74c53e1648e6be84d2a121b003d5625b8b634
Franky cunza chavez <franky.cunza@gmail.com>
2021-06-30 17:52:23 -0500

    changes

--- src/pages/profileData.jsx
+++ src/pages/profileData.jsx
@@ -42,11 +42,53 @@ const Profile = () => {
                                 </div>
                             </div>
                         )
-                    } else if (item.form_type == "text") {
+                    } else if (item.form_type == "full_name" || item.form_type == "paternal_surname" || item.form_type == "maternal_surname") {
                         return (
                             <div className="mt-2 text-left w-full" key={i}>
                                 <label htmlFor="" className="block mb-1">{item.title}</label>
                                 <input type="text" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                            </div>
+                        )
+                    } else if (item.form_type == "birth_day") {
+                        return (
+                            <div className="mt-2 text-left w-full" key={i}>
+                                <label htmlFor="" className="block mb-1">{item.title}</label>
+                                <input type="date" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                            </div>
+                        )
+                    } else if (item.form_type == "gender") {
+                        return (
+                            <div className="mt-2 text-left w-full" key={i}>
+                                <label htmlFor="" className="block mb-1">{item.title}</label>
+                                <input type="text" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                            </div>
+                        )
+                    } else if (item.form_type == "cell_phone") {
+                        return (
+                            <div className="mt-2 text-left w-full" key={i}>
+                                <label htmlFor="" className="block mb-1">{item.title}</label>
+                                <input type="number" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                            </div>
+                        )
+                    } else if (item.form_type == "gender") {
+                        return (
+                            <div className="mt-2 text-left w-full" key={i}>
+                                <label htmlFor="" className="block mb-1">{item.title}</label>
+                                <input type="text" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                            </div>
+                        )
+                    } else if (item.form_type == "email") {
+                        return (
+                            <div className="mt-2 text-left w-full" key={i}>
+                                <label htmlFor="" className="block mb-1">{item.title}</label>
+                                <input type="email" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
+                            </div>
+                        )
+                    } else if (item.form_type == "address") {
+                        return (
+                            <div className="mt-2 text-left w-full" key={i}>
+                                <label htmlFor="" className="block mb-1">{item.title}</label>
+                                <input type="address" name="" id="" className="h-10 rounded border border-blue-400 border-solid w-full pl-2" />
                             </div>
                         )
                     }
