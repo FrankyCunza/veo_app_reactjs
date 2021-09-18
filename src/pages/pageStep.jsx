2e5ec65638e29bcd1cacd71a89e919e545cca626
Franky Cunza <franky.cunza@prix.tips>
2021-09-18 18:28:47 -0500

    changes

--- src/pages/pageStep.jsx
+++ src/pages/pageStep.jsx
@@ -6,10 +6,24 @@ import { Link } from 'react-router-dom';
 const PageStep = () => {
     const location = useLocation();
     const [data, setData] = useState(location.state.data)
-    const [steps, setSteps] = useState(location.state.data.steps)
+    const [steps, setSteps] = useState([])
 
     const nextSlide = (index) => {
         document.getElementById("slides").style.transform = `translateX(-${index}00%)`
+    }
+
+    useEffect(() => {
+        let newArray = []
+        for (let item of data.steps) {
+            newArray.push({...item, status: null})
+        }
+        setSteps(newArray)
+    }, [])
+
+    const changeStep = (index, status) => {
+        let newArray = [...steps]
+        newArray[index]['status'] = status
+        setSteps(newArray)
     }
     return (
         <div className="max-w-3xl mx-auto">
@@ -26,13 +40,13 @@ const PageStep = () => {
                             <div className="bg-white rounded shadow-sm p-12 flex-shrink-0 w-full flex flex-col items-center justify-center" key={'step'+index}>
                                 <div className="w-20 h-20 bg-gray-200 rounded-full flex items-center justify-center"></div>
                                 <h3 className="mt-4">{el.instructions}</h3>
-                                <div className="mt-6">
-                                    <button type="button" className="bg-blue-600 text-white px-8 py-2 rounded-full">YES</button>
-                                    <button type="button" className="bg-blue-600 text-white px-8 py-2 rounded-full ml-4">NO</button>
+                                <div className={`mt-6`}>
+                                    <button type="button" className={`bg-blue-600 text-white px-8 py-2 rounded-full ${el.status ? '' : 'opacity-20'}`} onClick={() => {changeStep(index, true)}}>YES</button>
+                                    <button type="button" className={`bg-blue-600 text-white px-8 py-2 rounded-full ml-4 ${el.status==false ? '' : 'opacity-20'}`} onClick={() => {changeStep(index, false)}}>NO</button>
                                 </div>
                                 <div>
                                     <button className="bg-gray-300 px-12 py-3 rounded-full mt-4 text-gray-700" onClick={() => nextSlide(index-1)}>Back</button>
-                                    <button className="bg-blue-700 px-12 py-3 rounded-full mt-4 text-white ml-3" onClick={() => nextSlide(index+1)}>Next</button>
+                                    <button className={`bg-blue-700 px-12 py-3 rounded-full mt-4 text-white ml-3 ${steps[index].status !== null ? '' : 'opacity-20'}`} onClick={() => nextSlide(index+1)}>Next</button>
                                 </div>
                             </div>
                         )
