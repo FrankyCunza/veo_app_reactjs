dce236b3a45217554bbe35a66a7abe76363b88bb
Franky Cunza <franky.cunza@prix.tips>
2021-09-23 09:47:59 -0500

    add form from daasahboard

--- src/pages/auxiliaryControls.jsx
+++ src/pages/auxiliaryControls.jsx
@@ -124,8 +124,30 @@ const AuxiliaryControls = () => {
     })
 
     useEffect(() => {
+        getData()
         setLoading(false)
     }, [])
+
+    const getData = () => {
+        const param = {
+            company_id: localStorage.getItem('company_id'),
+            end_point: localStorage.getItem('end_point'),
+            page: 'auxiliary_control_form'
+        };
+        axios({
+            method: 'get',
+            url: 'http://localhost:8000/checkcards/get_check_cards',
+            headers: {
+                'security-header': 'Vim365Aputek/2020.04',
+                Authorization: localStorage.getItem('token'),
+                id: localStorage.getItem('id')
+            },
+            params: param
+        }).then(response => {
+            console.log(response.data)
+            setData(response.data)
+        }, [])
+    }
 
     const goToForm = (item) => {
         history.push("/auxiliaryControlsForm", {data: item})
